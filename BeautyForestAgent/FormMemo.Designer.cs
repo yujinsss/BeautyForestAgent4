3104bc4aae991a60a007a38632dd8bb30f8efa3c
Yujin Lee <lyj62346972@gmail.com>
2019-09-27 11:47:48 +0900

    #3 코드 구현 by 3214 이유진

--- BeautyForestAgent/FormMemo.Designer.cs
+++ BeautyForestAgent/FormMemo.Designer.cs
@@ -130,6 +130,7 @@
             this.btnLoadFileSelect.TabIndex = 1;
             this.btnLoadFileSelect.Text = "파일 선택";
             this.btnLoadFileSelect.UseVisualStyleBackColor = true;
+            this.btnLoadFileSelect.Click += new System.EventHandler(this.BtnLoadFileSelect_Click);
             // 
             // btnLoadFile
             // 
@@ -139,6 +140,7 @@
             this.btnLoadFile.TabIndex = 2;
             this.btnLoadFile.Text = "불러오기";
             this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.BtnLoadFile_Click);
             // 
             // txtLoadText
             // 
@@ -166,6 +168,7 @@
             this.btnSaveFileSelect.TabIndex = 1;
             this.btnSaveFileSelect.Text = "파일 선택";
             this.btnSaveFileSelect.UseVisualStyleBackColor = true;
+            this.btnSaveFileSelect.Click += new System.EventHandler(this.BtnSaveFileSelect_Click);
             // 
             // btnSaveFile
             // 
@@ -175,6 +178,7 @@
             this.btnSaveFile.TabIndex = 2;
             this.btnSaveFile.Text = "저장 하기";
             this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.BtnSaveFile_Click);
             // 
             // txtSaveText
             // 
--- BeautyForestAgent/FormMemo.cs
+++ BeautyForestAgent/FormMemo.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using System.IO;
 namespace BeautyForestAgent
 {
     public partial class FormMemo : Form
@@ -21,5 +21,75 @@ namespace BeautyForestAgent
             MessageBox.Show("건의사항이 보내졌습니다.", "건의사항 발송");
             this.Close();
         }
+
+        private void BtnLoadFileSelect_Click(object sender, EventArgs e)
+        {
+            DialogResult result = this.loadFileDlg.ShowDialog();
+            switch (result)
+            {
+                case DialogResult.OK:
+                    this.txtLoadFile.Text = this.loadFileDlg.FileName;
+                    break;
+                case DialogResult.Cancel:
+                    MessageBox.Show("취소하셨습니다", "알림");
+                    break;
+            }
+
+        }
+
+        private void BtnLoadFile_Click(object sender, EventArgs e)
+        {
+            if (txtLoadFile.Text == "")
+            {
+                MessageBox.Show("읽을 파일을 선택해 주세요", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (!File.Exists(txtLoadFile.Text))
+            {
+                MessageBox.Show("읽을 파일이 없습니다", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            using (StreamReader sr = new StreamReader(this.txtLoadFile.Text))
+            {
+                this.txtLoadText.Text = sr.ReadToEnd();
+            }
+
+        }
+
+        private void BtnSaveFileSelect_Click(object sender, EventArgs e)
+        {
+            DialogResult result = this.saveFileDlg.ShowDialog();
+            switch (result)
+            {
+                case DialogResult.OK:
+                    this.txtSaveFile.Text = this.saveFileDlg.FileName;
+                    break;
+                case DialogResult.Cancel:
+                    MessageBox.Show("취소하셨습니다", "알림");
+                    break;
+            }
+
+        }
+
+        private void BtnSaveFile_Click(object sender, EventArgs e)
+        {
+            if (txtSaveFile.Text == "")
+            {
+                MessageBox.Show("저장할 파일을 선택해 주세요", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(this.txtSaveFile.Text))
+                {
+                    sw.WriteLine(this.txtSaveText.Text);
+                }
+                MessageBox.Show("파일 저장 성공", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("파일 저장에 실패했습니다", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        
+    }
     }
 }
